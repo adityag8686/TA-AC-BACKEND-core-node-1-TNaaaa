bfbba8f600d0833fe31d324701cb0d59d4eeb33a
aditya <agoswami0708g@gmail.com>
2022-06-07 00:11:14 +0530

    done

---
+++ block-BNaaan/index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+    console.log(req.headers);
+    console.log(req.method, req.url);
+    res.end('Welcome');
+}
+server.listen(3000, () => {
+    console.log('listening 0n the prot 3000');
+});
